e6d5656eb51fcdf1a9bbdb061dbe5a34ed53456b
Richard Davenport <rad22684@gmail.com>
2018-09-09 00:09:51 -0500

    adding mackup config updating install

--- install.sh
+++ install.sh
@@ -17,12 +17,12 @@ brew bundle
 # Make ZSH the default shell environment
 chsh -s $(which zsh)
 
-# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
+# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .files
 rm -rf $HOME/.zshrc
-ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+ln -s $HOME/.files/.zshrc $HOME/.zshrc
 
 # Symlink the Mackup config file to the home directory
-ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
+ln -s $HOME/.files/.mackup.cfg $HOME/.mackup.cfg
 
 # Set macOS preferences
 # We will run this last because this will reload the shell
