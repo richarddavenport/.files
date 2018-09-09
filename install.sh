c74424d8efa5402341edd06f00b9ba3b33dd4f2c
Richard Davenport <rad22684@gmail.com>
2018-09-09 16:28:22 -0500

    mackup backup, updating Brewfile install.sh, adding brew.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,19 @@
 
 echo "Setting up your Mac..."
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+echo "Installing Xcode command line tools..."	
+sudo xcode-select --install
+sudo xcodebuild -license accept
+
+echo "Updating macOS..."
+sudo softwareupdate -i -a
+
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -17,12 +30,12 @@ brew bundle
 # Make ZSH the default shell environment
 chsh -s $(which zsh)
 
-# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .files
-rm -rf $HOME/.zshrc
-ln -s $HOME/.files/.zshrc $HOME/.zshrc
-
 # Symlink the Mackup config file to the home directory
+rm -rf $HOME/.mackup.cfg
 ln -s $HOME/.files/.mackup.cfg $HOME/.mackup.cfg
+
+# Run restore application settings
+mackup restore
 
 # Set macOS preferences
 # We will run this last because this will reload the shell
