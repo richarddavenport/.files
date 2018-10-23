688f8fb9dcb8786ce6325fa55761d560e532305e
Richard Davenport <rad22684@gmail.com>
2018-10-23 12:27:19 -0400

    adding in linuxbrew support, and fix for zsh breakage with brew install

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #! /usr/bin/env zsh
 
-# if macOS
-echo /usr/local/bin/zsh | sudo tee -a /etc/shells
+chsh -s /bin/zsh
+
+brew reinstall zsh
+
+# if macos and does not contain brew zsh as standard shell
+if [ "$(tail -1 /etc/shells)" != "/usr/local/bin/zsh" ]; then
+  echo /usr/local/bin/zsh | sudo tee -a /etc/shells
+fi
 
 chsh -s /usr/local/bin/zsh
