2f267af5739aca316171fb0ec550d68be529ea54
Richard Davenport <rad22684@gmail.com>
2019-02-05 21:43:51 -0500

    Fixing "invalid shell"

--- setup.sh
+++ setup.sh
@@ -15,17 +15,10 @@ fi
 brew install zsh
 brew install coreutils
 
-case `uname` in
-Darwin)
-  # if macos and does not contain brew zsh as standard shell
-  if [ "$(tail -1 /etc/shells)" != "/usr/local/bin/zsh" ]; then
-    echo $(which zsh) | sudo tee -a /etc/shells
-  fi
-;;
-*)
-    echo linux
-;;
-esac
+# if macos and does not contain brew zsh as standard shell
+if [ "$(tail -1 /etc/shells)" != $(which zsh) ]; then
+  echo $(which zsh) | sudo tee -a /etc/shells
+fi
 
 chsh -s $(which zsh)
 
