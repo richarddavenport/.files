614da1f49f32daaee39f0a54c11d80b8f0a6e43a
Richard Davenport <rad22684@gmail.com>
2019-04-20 15:04:02 -0500

    simple updates to setup and ubuntu

--- setup.sh
+++ setup.sh
@@ -8,6 +8,10 @@ if ! type "brew" > /dev/null; then
     ;;
     *)
       sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+      test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+      test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+      test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+      echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
     ;;
   esac
 fi
